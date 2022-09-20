93224f03645ceac4b7f9db364418f65f3a2a0bc4
Rahmat Ramadhan <rahmat.rama98@gmail.com>
2022-09-20 16:03:38 +0700

    bulk request bearer

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,6 +19,13 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	// https://mvnrepository.com/artifact/org.springframework.boot/spring-boot-starter-web
+	implementation("org.springframework.boot:spring-boot-starter-web:2.7.3")
+	// https://mvnrepository.com/artifact/org.apache.poi/poi-ooxml
+	implementation("org.apache.poi:poi-ooxml:3.9")
+// https://mvnrepository.com/artifact/org.apache.poi/poi
+	implementation("org.apache.poi:poi:5.2.3")
+
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
--- src/main/kotlin/rahmat/rmdn/generatebearer/GenerateBearerApplication.kt
+++ src/main/kotlin/rahmat/rmdn/generatebearer/GenerateBearerApplication.kt
@@ -1,7 +1,21 @@
 package rahmat.rmdn.generatebearer
 
+import org.apache.commons.io.IOUtils
+import org.apache.poi.xssf.usermodel.XSSFWorkbook
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.http.HttpEntity
+import org.springframework.http.HttpHeaders
+import org.springframework.http.MediaType
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.client.RestTemplate
+import java.io.File
+import java.io.FileInputStream
+import java.io.FileOutputStream
+import java.util.*
+import javax.servlet.http.HttpServletResponse
 
 @SpringBootApplication
 class GenerateBearerApplication
@@ -9,3 +23,112 @@ class GenerateBearerApplication
 fun main(args: Array<String>) {
 	runApplication<GenerateBearerApplication>(*args)
 }
+
+@RestController
+internal class EmployeeController() {
+
+	@PostMapping("/bulk-bearer")
+	fun bulkBearer(@RequestBody users: List<User?>, httpServletResponse: HttpServletResponse): ByteArray {
+		val data = mutableListOf<MutableMap<String, String?>>()
+		users.forEach { it ->
+			val item = mutableMapOf<String, String?>()
+			item["username"] = it?.username
+			item["password"] = it?.password
+
+			val token = requestBearer(it?.password, it?.username)
+			item["bearer"] = token
+
+			data.add(item)
+		}
+		return writeToExcelFile(data,httpServletResponse, "excelnya")
+	}
+}
+
+fun requestBearer(pass:String?, username: String?): String? {
+	try {
+		val restTemplate = RestTemplate()
+
+		val url = "http://localhost:8080/sso"
+		val requestJson = "{\"username\":\"$username\", \"password\":\"$pass\"}"
+		val headers = HttpHeaders()
+		headers.contentType = MediaType.APPLICATION_JSON
+		val entity = HttpEntity(requestJson, headers)
+		val response = restTemplate.postForObject(url, entity, ResponseGetBulkBearer::class.java)
+
+		if (response?.success == true) {
+			return response.data?.access_token
+		}
+		return null
+	} catch (e: Exception) {
+		e.printStackTrace()
+		return null
+	}
+}
+
+fun writeToExcelFile(
+	data: MutableList<MutableMap<String, String?>>, httpServletResponse: HttpServletResponse, fileName: String
+): ByteArray {
+	val xlWb = XSSFWorkbook()
+	val xlWs = xlWb.createSheet()
+
+	// Create headers
+	val keys = data.first().keys
+	val headerRow = xlWs.createRow(0)
+	keys.forEachIndexed { index, key ->
+		headerRow.createCell(index).setCellValue(key)
+	}
+
+	// Insert data
+	data.forEachIndexed { index, data ->
+		keys.forEachIndexed { columnIndex, s ->
+			xlWs.createRow(index + 1).createCell(columnIndex).setCellValue(data[s].toString())
+		}
+	}
+
+	val dir = File(System.getProperty("java.io.tmpdir") + getTimeDir())
+	if (!dir.exists()) {
+		dir.mkdirs()
+	}
+
+	val filepath = dir.absolutePath + File.separator + fileName + System.currentTimeMillis() + ".xlsx"
+
+	val outputStream = FileOutputStream(filepath)
+	xlWb.write(outputStream)
+
+	val outputFile = File(filepath)
+
+	httpServletResponse.setHeader("Content-Disposition", "attachment; filename=" + outputFile.name)
+	val fis = FileInputStream(outputFile)
+	return IOUtils.toByteArray(fis)
+}
+
+fun getTimeDir(): String {
+	return try {
+		val cal = Calendar.getInstance()
+		cal.time = Date()
+		cal.get(Calendar.YEAR)
+			.toString() + File.separator + (cal.get(Calendar.MONTH) + 1).toString() + File.separator + cal.get(
+			Calendar.DAY_OF_MONTH
+		).toString() + File.separator
+	} catch (ex: Exception) {
+		""
+	}
+}
+
+
+// entity
+data class User(
+	val username: String?,
+	val password: String?
+)
+
+data class ResponseGetBulkBearer(
+	val success: Boolean?,
+	val message: String?,
+	val data: Data?
+)
+
+data class Data(
+	val access_token: String?
+)
+
