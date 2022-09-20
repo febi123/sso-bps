a2f213afdb1dad4cc9020a6e9be8e626d9c93e50
Rahmat Ramadhan <rahmat.rama98@gmail.com>
2022-09-20 08:58:29 +0700

    initial generate bulk bearer

---
+++ src/main/kotlin/rahmat/rmdn/generatebearer/GenerateBearerApplication.kt
@@ -0,0 +1,11 @@
+package rahmat.rmdn.generatebearer
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GenerateBearerApplication
+
+fun main(args: Array<String>) {
+	runApplication<GenerateBearerApplication>(*args)
+}
